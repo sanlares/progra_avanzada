66992b899b30155b68cd4893b7a33349e5896c19
sanlares <slares.gm@gmail.com>
2024-05-08 23:47:53 -0300

    cambios en conexion recomm

--- main.py
+++ main.py
@@ -7,29 +7,68 @@ import os
 
 app = FastAPI()
 
-@app.get("/recommendations/{ADV}/{Modelo}") 
-def recommendations(ADV: int, Modelo: str):
+from fastapi import FastAPI, HTTPException
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from datetime import datetime
+from psycopg2 import pool
+import os
 
-    #me conecto a la base de datos
-    conn = psycopg2.connect(
-        dbname='postgres',
-        user='postgres',
-        password='conesacolegiales',
-        host='programacion.cpusky0oqvsv.us-east-2.rds.amazonaws.com',
-        port=5432
-    )
-    cur = conn.cursor()
+class Database:
+    # Clase singleton para manejar el pool de conexiones
+    _connection_pool = None
 
-    today_date = datetime.now().strftime('%Y-%m-%d')
+    @staticmethod
+    def initialize():
+        Database._connection_pool = pool.SimpleConnectionPool(
+            1, # mínimo número de conexiones
+            10, # máximo número de conexiones
+            user=os.getenv("DB_USER", "postgres"),
+            password=os.getenv("DB_PASSWORD", "pass"),
+            host=os.getenv("DB_HOST", "programacion.cpusky0oqvsv.us-east-2.rds.amazonaws.com"),
+            port=os.getenv("DB_PORT", "5432"),
+            database=os.getenv("DB_NAME", "postgres")
+        )
 
-    if Modelo=="TopCTR":
-        table="top_ctr_table"
-    else:
-        table="top_products_table"
-    
-    recomm_table=cur.execute(f'select product_id from {table} where advertiser_id={ADV} and date={today_date}')
+    @staticmethod
+    def get_connection():
+        return Database._connection_pool.getconn()
 
-    return recomm_table
+    @staticmethod
+    def return_connection(connection):
+        Database._connection_pool.putconn(connection)
+
+    @staticmethod
+    def close_all_connections():
+        Database._connection_pool.closeall()
+
+# Inicializa el pool al iniciar la aplicación
+Database.initialize()
+
+@app.get("/recommendations/{ADV}/{Modelo}")
+def recommendations(ADV: str, Modelo: str):
+    print("Connecting to DB")
+    conn = Database.get_connection()
+    try:
+        cur = conn.cursor()
+        print("Connected to DB")
+
+        today_date = datetime.now().strftime('%Y-%m-%d')
+        table = "top_ctr_table" if Modelo == "TopCTR" else "top_products_table"
+        
+        print("Executing Query")
+        query = f"SELECT product_id FROM {table} WHERE advertiser_id = %s AND date = %s;"
+        cur.execute(query, (ADV, today_date))
+        recomm_table = cur.fetchall()
+        print("Query Executed")
+
+        return {"data": recomm_table}
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    finally:
+        cur.close()
+        Database.return_connection(conn)
 
 @app.get("/stats/") 
 def stats(): 
