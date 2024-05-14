d27bfb5bd76f1d346dfa77bde1870d3d79c3f363
sanlares <santiago.laresharbin@ext.myfitnesspal.com>
2024-05-14 10:44:02 -0300

    sacando librerias

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,4 +1,3 @@
-import numpy as np
 from datetime import datetime
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
@@ -9,18 +8,10 @@ from datetime import timedelta
 import psycopg2.extras 
 import psycopg2
 from datetime import timedelta
-import pandas as pd
-from io import StringIO
-import boto3
 
 app = FastAPI()
 
-// q: increase the quality of the code?
-// generate a .env file for my passwords
-// create a new file called .env
-
-
-
+load_dotenv()
 
 class Database:
     #clase singleton para manejar el pool de conexiones
@@ -29,13 +20,19 @@ class Database:
     @staticmethod
     def initialize(): #se conecta
         Database._connection_pool = pool.SimpleConnectionPool(
-            1, # mínimo número de conexiones
-            10, # máximo número de conexiones
-        dbname='',
-        user='',
-        password='',
-        host='',
-        port=''
+            1,
+            10,
+            #// replace with strings, not env vaiables
+            dbname=#'postgres',#
+            os.getenv('DB_NAME'),
+            user=#'postgres',#
+            os.getenv('DB_USER'),
+            password=#'conesacolegiales',#
+            os.getenv('DB_PASS'),
+            host=#'tpprogramacion.cpusky0oqvsv.us-east-2.rds.amazonaws.com',#
+            os.getenv('DB_HOST'),
+            port=#5432#
+            os.getenv('DB_PORT')
         )
 
     @staticmethod
@@ -66,7 +63,7 @@ def recommendations(advertiser_id: str, Modelo: str):
         yesterday_date = (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d')
         if Modelo == 'TopCTR':
                 table = 'top_ctr_table'
-        elif Modelo =='ToProduct':
+        elif Modelo =='TopProduct':
                 table = 'top_products_table'
         else:
             raise HTTPException(status_code=400, detail=f"Modelo desconocido: {Modelo}")
@@ -77,8 +74,11 @@ def recommendations(advertiser_id: str, Modelo: str):
                
         print("Query Executed")
         return {"data": recomm_table}
+    except psycopg2.DatabaseError as e:
+        print(f"Database error occurred: {e}")
+        raise HTTPException(status_code=500, detail="Database Error")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An unexpected error occurred: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
     finally:
         if cur:
@@ -94,13 +94,25 @@ def statstistics_summary():
         conn = Database.get_connection()
         cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         
-        yesterday_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+        yesterday_date = (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d')
+    
+    except Exception as e:
+        print(f"An error occurred while connecting to the database: {e}")
+        raise HTTPException(status_code=500,detail="Internal Server Error")
 
-#query para contar la cantidad de advertisers unicos en ambas tablas
-        cur.execute(f"SELECT COUNT(DISTINCT advertiser_id) AS total_advertisers_for_ctr_and_products FROM top_ctr_table WHERE date ='{yesterday_date}' UNION ALL SELECT COUNT(DISTINCT advertiser_id) FROM top_products_table WHERE date='{yesterday_date}'")
+    try:
+        cur.execute(f"""SELECT COUNT(DISTINCT advertiser_id) AS total_advertisers_for_ctr_and_products 
+                    FROM top_ctr_table WHERE date ='{yesterday_date}' 
+                    UNION ALL 
+                    SELECT COUNT(DISTINCT advertiser_id) 
+                    FROM top_products_table 
+                    WHERE date='{yesterday_date}'""")
         total_advertisers_for_ctr_and_products = cur.fetchall()
-        
-#query para estimar el top 5  de ctr 
+    except Exception as e:
+        print(f"An error occurred while executing total_advertisers_for_ctr_and_products query: {e}")
+        raise HTTPException(status_code=500,detail=str(e))
+
+    try:
         cur.execute("""
                     SELECT advertiser_id, product_id, CTR AS max_ctr
                     FROM top_ctr_table
@@ -108,41 +120,44 @@ def statstistics_summary():
                     LIMIT 5
                     """)
         results = cur.fetchall()
+    except Exception as e:
+        print(f"An error occurred while executing top5_ctr query: {e}")
+        raise HTTPException(status_code=500,detail=str(e))
 
 #query para estimar el top 5  de productos
-        cur.execute("""
-                    SELECT advertiser_id, product_id, visitas AS max_products
-                    FROM top_products_table
-                    ORDER BY visitas DESC
-                    LIMIT 5
-                    """)
-        results_products = cur.fetchall()
-
-#promedio de ctr y visitas esto nos da una idea de que tan bien esta realizando los anunciantes y los productos. 
-        cur.execute("""
-                    SELECT advertiser_id, product_id, AVG(CTR) AS average_ctr
-                    FROM top_ctr_table
-                    GROUP BY advertiser_id, product_id
-                    ORDER BY average_ctr DESC
-                    LIMIT 5
-                    """)
-        average_ctr = cur.fetchall()
-        
-        cur.execute("""
-                    SELECT advertiser_id, product_id, AVG(visitas) AS visitas_average
-                    FROM top_products_table
-                    GROUP BY advertiser_id, product_id
-                    ORDER BY visitas_average DESC
-                    LIMIT 5
-                    """)
-        visitas_average= cur.fetchall()
+    #    cur.execute("""
+    #                SELECT advertiser_id, product_id, visitas AS max_products
+    #                FROM top_products_table
+    #                ORDER BY visitas DESC
+    #                LIMIT 5
+    #                """)
+    #    results_products = cur.fetchall()
+#
+#pro#medio de ctr y visitas esto nos da una idea de que tan bien esta realizando los anunciantes y los productos. 
+    #    cur.execute("""
+    #                SELECT advertiser_id, product_id, AVG(CTR) AS average_ctr
+    #                FROM top_ctr_table
+    #                GROUP BY advertiser_id, product_id
+    #                ORDER BY average_ctr DESC
+    #                LIMIT 5
+    #                """)
+    #    average_ctr = cur.fetchall()
+    #    
+    #    cur.execute("""
+    #                SELECT advertiser_id, product_id, AVG(visitas) AS visitas_average
+    #                FROM top_products_table
+    #                GROUP BY advertiser_id, product_id
+    #                ORDER BY visitas_average DESC
+    #                LIMIT 5
+    #                """)
+    #    visitas_average= cur.fetchall()"""
         
         return{
             "total_advertisers_for_ctr_and_products":[total['total_advertisers_for_ctr_and_products'] for total in total_advertisers_for_ctr_and_products],
             "top5_ctr":results,
-            "top5_products":results_products,
-            "average_ctr":average_ctr,
-            "visitas_average":visitas_average
+            #"top5_products":results_products,
+            #"average_ctr":average_ctr,
+            #"visitas_average":visitas_average
         }
     except Exception as e:
         print(f"An error occurred:{e}")
@@ -163,7 +178,7 @@ def get_history(advertiser_id:str):
         
         yesterday_date = (datetime.now() - timedelta(days=1))
         
-        #calcular la fecha de gace 7 dias
+        #calcular la fecha de hace 7 dias
         seven_days_ago= yesterday_date - timedelta(days=7)
         yesterday_date= yesterday_date.strftime('%Y-%m-%d')
         formatted_date = seven_days_ago.strftime('%Y-%m-%d')
